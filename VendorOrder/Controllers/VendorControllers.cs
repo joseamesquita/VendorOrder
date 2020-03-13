823aba056e2972fd20d6571b516e853922abc21c
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-13 14:09:49 -0700

    vendor controllers

---
+++ VendorOrder/Controllers/VendorControllers.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendorOrder.Models;
+
+namespace VendorOrder.Controllers
+{
+  public class VendorsController : Controller
+  {
+    //index
+    [HttpGet("/vendors")]
+    public ActionResult VendorsIndex()
+    {
+      List<VendorOrder> showList = Vendor.Board;
+      return View(showList);
+    }
+
+    //New
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+
+      return View();
+    }
+
+    //Create
+    [HttpPost("/vendors/create")]
+    public ActionResult Create(string title, string description, string contact)
+    {
+      Vendor order = new Vendor(title, description, contact);
+      return RedirectToAction("VendorsIndex");
+    }
+
+    //Show
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor order = Vendor.Find(id);
+      return View(order);
+    }
+
+    //Edit form
+    [HttpGet("/vendors/{id}/edit")]
+    public ActionResult Edit(int id)
+    {
+      Vendor order = Vendor.Find(id);
+      return View(order);
+    }
+
+    //Update
+    [HttpPost("/vendors/{id}")]
+    public ActionResult Update(int id, string title, string description, string contact)
+    {
+      Jobs.UpdateOrder(id, title, description, contact);
+      return RedirectToAction("VendorsIndex");
+    }
+
+    //Destroy
+    [HttpPost("/vendors/delete/{id}")]
+    public ActionResult Delete(int id)
+    {
+      Vendor.DeleteOrder(id);
+      return View();
+    }
+  }
+
+}
